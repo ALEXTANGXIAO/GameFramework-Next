e3dc3476b4ce45975c4ecabeaf0870ae2ef52ef5
ALEXTANG <574809918@qq.com>
2024-02-02 10:36:41 +0800

    Luban-next
    Luban-next

---
+++ Configs/GameConfig/CustomTemplate/ConfigSystem.cs
@@ -0,0 +1,60 @@
+using Luban;
+using GameBase;
+using GameConfig;
+using UnityEngine;
+using UnityGameFramework;
+
+namespace GameProto
+{
+    /// <summary>
+    /// 配置加载器。
+    /// </summary>
+    public class ConfigSystem : Singleton<ConfigSystem>
+    {
+        private bool _init = false;
+
+        private Tables _tables;
+
+        public Tables Tables
+        {
+            get
+            {
+                if (!_init)
+                {
+                    Load();
+                }
+
+                return _tables;
+            }
+        }
+
+        /// <summary>
+        /// 加载配置。
+        /// </summary>
+        public void Load()
+        {
+            _tables = new Tables(LoadByteBuf);
+            _init = true;
+        }
+
+        /// <summary>
+        /// 加载二进制配置。
+        /// </summary>
+        /// <param name="file">FileName</param>
+        /// <returns>ByteBuf</returns>
+        private ByteBuf LoadByteBuf(string file)
+        {
+            TextAsset textAsset = null;
+            textAsset = GameModule.Resource.GetPreLoadAsset<TextAsset>(file);
+            if (textAsset != null)
+            {
+                return new ByteBuf(textAsset.bytes);
+            }
+            else
+            {
+                textAsset = GameModule.Resource.LoadAsset<TextAsset>(file);
+                return new ByteBuf(textAsset.bytes);
+            }
+        }
+    }
+}
---
+++ Configs/GameConfig/gen_code_bin_to_project.bat
@@ -0,0 +1,17 @@
+set WORKSPACE=../..
+set LUBAN_DLL=%WORKSPACE%\Tools\Luban\Luban.dll
+set CONF_ROOT=.
+set DATA_OUTPATH=%WORKSPACE%/UnityProject/Assets/AssetRaw/Configs/bytes/
+set CODE_OUTPATH=%WORKSPACE%/UnityProject/Assets/GameScripts/HotFix/GameProto/GameConfig/
+
+xcopy /s /e /i /y "%CONF_ROOT%\CustomTemplate\ConfigSystem.cs" "%WORKSPACE%\UnityProject\Assets\GameScripts\HotFix\GameProto\ConfigSystem.cs"
+
+dotnet %LUBAN_DLL% ^
+    -t client ^
+    -c cs-bin ^
+    -d bin^
+    --conf %CONF_ROOT%\luban.conf ^
+    -x outputCodeDir=%CODE_OUTPATH% ^
+    -x outputDataDir=%DATA_OUTPATH% 
+pause
+
