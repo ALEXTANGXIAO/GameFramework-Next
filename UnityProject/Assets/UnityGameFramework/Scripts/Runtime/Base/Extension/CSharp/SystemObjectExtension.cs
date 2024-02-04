b33ca1bb62f2929bc5f777ae3fe27e308a13fd7c
ALEXTANG <574809918@qq.com>
2024-02-04 17:58:53 +0800

    Internal Extension
    Internal Extension

---
+++ UnityProject/Assets/UnityGameFramework/Scripts/Runtime/Base/Extension/CSharp/SystemObjectExtension.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace UnityGameFramework.Runtime
+{
+    public static class SystemObjectExtension
+    {
+        public static T Self<T>(this T self, Action<T> onDo)
+        {
+            onDo?.Invoke(self);
+            return self;
+        }
+
+        public static T Self<T>(this T self, Func<T, T> onDo)
+        {
+            return onDo.Invoke(self);
+        }
+
+        public static bool IsNull<T>(this T selfObj) where T : class
+        {
+            return null == selfObj;
+        }
+
+        public static bool IsNotNull<T>(this T selfObj) where T : class
+        {
+            return null != selfObj;
+        }
+
+        public static T As<T>(this object selfObj) where T : class
+        {
+            return selfObj as T;
+        }
+    }
+}
---
+++ UnityProject/Assets/UnityGameFramework/Scripts/Runtime/Base/Extension/CSharp/SystemStringExtension.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace UnityGameFramework.Runtime
+{
+    public static class SystemStringExtension
+    {
+        public static bool IsNullOrEmpty(this string selfStr)
+        {
+            return string.IsNullOrEmpty(selfStr);
+        }
+
+        public static bool IsNotNullAndEmpty(this string selfStr)
+        {
+            return !string.IsNullOrEmpty(selfStr);
+        }
+
+        public static bool IsTrimNullOrEmpty(this string selfStr)
+        {
+            return selfStr == null || string.IsNullOrEmpty(selfStr.Trim());
+        }
+
+        public static bool IsTrimNotNullAndEmpty(this string selfStr)
+        {
+            return selfStr != null && !string.IsNullOrEmpty(selfStr.Trim());
+        }
+
+        private static readonly char[] mCachedSplitCharArray = { '.' };
+
+        public static string[] Split(this string selfStr, char splitSymbol)
+        {
+            mCachedSplitCharArray[0] = splitSymbol;
+            return selfStr.Split(mCachedSplitCharArray);
+        }
+
+        public static string FillFormat(this string selfStr, params object[] args)
+        {
+            return string.Format(selfStr, args);
+        }
+
+        public static StringBuilder Builder(this string selfStr)
+        {
+            return new StringBuilder(selfStr);
+        }
+
+        public static StringBuilder AddPrefix(this StringBuilder self, string prefixString)
+        {
+            self.Insert(0, prefixString);
+            return self;
+        }
+
+        public static int ToInt(this string selfStr, int defaultValue = 0)
+        {
+            var retValue = defaultValue;
+            return int.TryParse(selfStr, out retValue) ? retValue : defaultValue;
+        }
+
+        public static DateTime ToDateTime(this string selfStr, DateTime defaultValue = default(DateTime))
+        {
+            return DateTime.TryParse(selfStr, out var retValue) ? retValue : defaultValue;
+        }
+
+
+        public static float ToFloat(this string selfStr, float defaultValue = 0)
+        {
+            return float.TryParse(selfStr, out var retValue) ? retValue : defaultValue;
+        }
+
+        public static bool HasChinese(this string input)
+        {
+            return Regex.IsMatch(input, @"[\u4e00-\u9fa5]");
+        }
+
+        public static bool HasSpace(this string input)
+        {
+            return input.Contains(" ");
+        }
+
+        public static string RemoveString(this string str, params string[] targets)
+        {
+            return targets.Aggregate(str, (current, t) => current.Replace(t, string.Empty));
+        }
+
+        public static string StringJoin(this IEnumerable<string> self, string separator)
+        {
+            return string.Join(separator, self);
+        }
+    }
+}
