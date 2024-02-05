03ac987559c67bb06251b6ce58c64a861ddf680c
ALEXTANG <574809918@qq.com>
2024-02-05 12:28:43 +0800

    Update StringExtension.cs

--- UnityProject/Assets/UnityGameFramework/Scripts/Runtime/Utility/StringExtension.cs
+++ UnityProject/Assets/UnityGameFramework/Scripts/Runtime/Utility/StringExtension.cs
@@ -1,59 +1,62 @@
-﻿/// <summary>
-/// 对 string 的扩展方法。
-/// </summary>
-public static class StringExtension
+﻿namespace UnityGameFramework.Runtime
 {
     /// <summary>
-    /// 从指定字符串中的指定位置处开始读取一行。
+    /// 对 string 的扩展方法。
     /// </summary>
-    /// <param name="rawString">指定的字符串。</param>
-    /// <param name="position">从指定位置处开始读取一行，读取后将返回下一行开始的位置。</param>
-    /// <returns>读取的一行字符串。</returns>
-    public static string ReadLine(this string rawString, ref int position)
+    public static class StringExtension
     {
-        if (position < 0)
+        /// <summary>
+        /// 从指定字符串中的指定位置处开始读取一行。
+        /// </summary>
+        /// <param name="rawString">指定的字符串。</param>
+        /// <param name="position">从指定位置处开始读取一行，读取后将返回下一行开始的位置。</param>
+        /// <returns>读取的一行字符串。</returns>
+        public static string ReadLine(this string rawString, ref int position)
         {
-            return null;
-        }
-
-        int length = rawString.Length;
-        int offset = position;
-        while (offset < length)
-        {
-            char ch = rawString[offset];
-            switch (ch)
+            if (position < 0)
             {
-                case '\r':
-                case '\n':
-                    if (offset > position)
-                    {
-                        string line = rawString.Substring(position, offset - position);
-                        position = offset + 1;
-                        if ((ch == '\r') && (position < length) && (rawString[position] == '\n'))
+                return null;
+            }
+
+            int length = rawString.Length;
+            int offset = position;
+            while (offset < length)
+            {
+                char ch = rawString[offset];
+                switch (ch)
+                {
+                    case '\r':
+                    case '\n':
+                        if (offset > position)
                         {
-                            position++;
+                            string line = rawString.Substring(position, offset - position);
+                            position = offset + 1;
+                            if ((ch == '\r') && (position < length) && (rawString[position] == '\n'))
+                            {
+                                position++;
+                            }
+
+                            return line;
                         }
 
-                        return line;
-                    }
+                        offset++;
+                        position++;
+                        break;
 
-                    offset++;
-                    position++;
-                    break;
-
-                default:
-                    offset++;
-                    break;
+                    default:
+                        offset++;
+                        break;
+                }
             }
-        }
 
-        if (offset > position)
-        {
-            string line = rawString.Substring(position, offset - position);
-            position = offset;
-            return line;
-        }
+            if (offset > position)
+            {
+                string line = rawString.Substring(position, offset - position);
+                position = offset;
+                return line;
+            }
 
-        return null;
+            return null;
+        }
     }
 }
