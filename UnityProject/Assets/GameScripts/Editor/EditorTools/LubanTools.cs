91878c6e27b82b648ec3daeea464e65707081fc4
ALEXTANG <574809918@qq.com>
2024-02-22 14:30:30 +0800

    LubanTools

---
+++ UnityProject/Assets/GameScripts/Editor/EditorTools/LubanTools.cs
@@ -0,0 +1,14 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace GameScripts.Editor
+{
+    public static class LubanTools
+    {
+        [MenuItem("Game Framework/Tools/Luban 转表")]
+        public static void BuildLubanExcel()
+        {
+            Application.OpenURL(Application.dataPath + @"/../../Configs/GameConfig/gen_code_bin_to_project_lazyload.bat");
+        }
+    }
+}
